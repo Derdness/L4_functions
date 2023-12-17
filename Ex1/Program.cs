924607482773518f4ae12d193fec2fb46eebc7f7
Александр Романов <derdness@yandex.ru>
2023-12-18 01:13:44 +0700

    Решение задачи 1 по теме функции
    Задача 1: Напишите программу, которая бесконечно
    запрашивает целые числа с консоли.
    Программа завершается при вводе символа ‘q’
    или при вводе числа, сумма цифр которого чётная.

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 1: Напишите программу, которая бесконечно 
+запрашивает целые числа с консоли. 
+Программа завершается при вводе символа ‘q’ 
+или при вводе числа, сумма цифр которого чётная.
+*/
+bool flag = true; //Условие завершения программы
+int digit;
+while(flag)
+{
+    Console.Write("Введите целое число: ");
+    string input = Console.ReadLine();
+    if(input == "q") //Если введено q, закончить выполнение 
+    {
+        flag = false;
+        continue;
+    }
+    try
+    {
+        digit = Convert.ToInt32(input);
+        int sum = 0;
+        do //Найдём сумму цифр числа
+        {
+            sum += digit % 10;
+            digit = digit / 10;
+        } while(digit / 10 != 0);
+        if(sum % 2 == 0) //Если сумма чётная завершить программу
+            flag = false;
+    }
+    catch(FormatException e){
+        Console.WriteLine("Это не целое число.");
+    }
+}
